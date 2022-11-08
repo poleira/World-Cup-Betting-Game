f6ea5622bc101a96537d4d0131a788b1427d32e1
Marco Polo Perciano Pereira <marcoperciano@gmail.com>
2022-11-08 01:28:55 -0300

    pagina painel com 1 grupo completo

--- Bolao (frontend)/src/app/painel/lista-paineis/lista-paineis.component.ts
+++ Bolao (frontend)/src/app/painel/lista-paineis/lista-paineis.component.ts
@@ -1,78 +1,78 @@
+import { GrupoA } from './Interfaces/grupo-a';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { HeaderComponent } from 'src/app/header/header.component';
-import { GrupoA } from 'src/app/painel/lista-paineis/Interfaces/grupo-a';
 import { __await } from 'tslib';
 
 @Component({
   selector: 'app-lista-paineis',
   templateUrl: './lista-paineis.component.html',
   styleUrls: ['./lista-paineis.component.css'],
-  providers:[HeaderComponent]
+  providers: [HeaderComponent],
 })
 export class ListaPaineisComponent implements OnInit {
-  Usuario: string = "";
+  Usuario: string = '';
 
-  GrupoA = {
-    Qatar: 0,
-    Equador: 0,
-    Senegal: 0,
-    Holanda: 0,
-  };
-  ArrayA: string []= [];
-  data: any; 
+  GrupoA: any;
+  ArrayA: any[] = [];
+  data: any;
 
-  constructor(private jwtHelper: JwtHelperService, private router: Router, private http:  HttpClient) { }
-  
+  constructor(
+    private jwtHelper: JwtHelperService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
   ngOnInit(): void {
-    this.getUsuario ();
-    this.http.post<any>('https://localhost:7288/api/Painel', { usuario: this.Usuario }).subscribe(data => {
-        this.data = data, this.GrupoA.Equador = data.equador, this.GrupoA.Qatar = data.qatar, 
-        this.GrupoA.Holanda = data.holanda, this.GrupoA.Senegal = data.senegal 
-      })
-    this.Organize();    
+    this.getUsuario();
+    this.http
+      .post<any>('https://localhost:7288/api/Painel', { usuario: this.Usuario })
+      .subscribe((data) => {
+        this.data = data;
+        this.GrupoA = {
+          equador: data.equador,
+          qatar: data.qatar,
+          holanda: data.holanda,
+          senegal: data.senegal,
+        };
+        this.Organize();
+      });
   }
 
-  Organize () {
-      var arrayA = Object.values(this.GrupoA);
-      arrayA.sort();
-      var count:number = 0;
-      for (const [key, value] of Object.entries(this.GrupoA)) {
-        if (value == arrayA[count]){
-          this.ArrayA[count] = key;
-        }
-        count = count +1
-      }
-      };
-  
-  
+  getPontos(pais: any) {
+    return this.data[`${pais}Pontos`];
+  }
+
+  Organize() {
+    console.log(this.GrupoA);
+    this.ArrayA = Object.keys(this.GrupoA).sort(
+      (a, b) => this.GrupoA[a] - this.GrupoA[b]
+    );
+
+    console.log(this.ArrayA);
+  }
+
   navegarGrupoA = () => {
-      this.router.navigate(['grupoA']);
-    }
-  
+    this.router.navigate(['grupoA']);
+  };
+
   isUserAuthenticated = (): boolean => {
-    const token = localStorage.getItem("jwt");
-    if (token && !this.jwtHelper.isTokenExpired(token)){
+    const token = localStorage.getItem('jwt');
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
       return true;
     }
     return false;
-  }
+  };
 
-   getUsuario () {
+  getUsuario() {
     const token = localStorage.getItem('jwt');
 
     if (token) {
-    const decoded = this.jwtHelper.decodeToken(token);
+      const decoded = this.jwtHelper.decodeToken(token);
       this.Usuario =
         decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
+    }
   }
 }
-
-
-  
-  
-  
-}
-
